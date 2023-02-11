69166459421e3ebe7f46cce73082f41c6f5c1da6
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-10 20:22:04 -0500

    Operador Logico NOT

--- main.js
+++ main.js
@@ -6,11 +6,9 @@
 // valores lógicos o asimilables a ellos
 
 let a = confirm("Variable A\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False")
-let b = confirm("Variable B\n\t\tSeleciona Aceptar si deseas el boleano True\n\t\tSeleciona Cancelar si deseas el boleano False")
 
-let cuerpo = document.querySelector("#myOR");
+let cuerpo = document.querySelector("#myNOT");
 cuerpo.insertAdjacentHTML("beforeend", ` <tr>
                                             <td>${a}</td>
-                                            <td>${b}</td>
-                                            <td>${a || b}</td>
+                                            <td>${!a}</td>
                                         </tr>`);
